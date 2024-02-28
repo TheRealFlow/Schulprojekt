d52bc545aa9674ecd39b93f39397604dc6f889c9
florianludwig <fludwig@kpler.com>
2024-02-28 10:29:30 +0100

    mooooooore Frontend stuff und so

--- versandplanung/articles/views.py
+++ versandplanung/articles/views.py
@@ -1,31 +1,20 @@
 from django.shortcuts import render
+from openpyxl import load_workbook
 
 from versandplanung.articles.models import Article
 
 
 def view_articles(request):
-    """articles = [
-        {
-            'id': '1',
-            'name': 'Bread',
-            'price': 1.50,
-            'short_description': 'Awesome Product'
-        },
-        {
-            'id': '2',
-            'name': 'Milk',
-            'price': 2.50,
-            'short_description': 'Awesome Product'
-        },
-        {
-            'id': '3',
-            'name': 'Eggs',
-            'price': 3.50,
-            'short_description': 'Awesome Product'
-        }
-    ]"""
-    
-    # articles = Article.objects.all() todo make reader form csv/xlsx file
+    articles = []
+    articles_workbook = load_workbook('datasources/articles.xlsx')
+    article_sheet = articles_workbook.active
+
+    # min_row=2 because the first row contains the headers
+    for row in article_sheet.iter_rows(min_row=2, max_row=article_sheet.max_row, values_only=True):
+        articles.append(
+            Article(id=row[0], price=row[1], name=row[2], short_description=row[3]))
+
+    articles_workbook.close()
 
     return render(request, 'articles.html', {
         'articles': articles
