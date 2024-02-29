fae8c51342a706112ee4ad620da8f475e04197c2
Ben Janowski <janowski@fleetmon.com>
2024-02-29 12:19:03 +0100

    feat: use max vehicle count

--- versandplanung/orders/management/commands/generate_route.py
+++ versandplanung/orders/management/commands/generate_route.py
@@ -10,16 +10,15 @@ class Command(BaseCommand):
     help = 'Generates the delivery routes for the next day.'
     
     def add_arguments(self, parser: CommandParser) -> None:
-        parser.add_argument('--vehicle_count', type=int, help='The number of vehicles to use for the delivery routes.')
+        parser.add_argument('--max_vehicle_count', type=int, help='The number of vehicles to use for the delivery routes.')
         parser.add_argument('--warehouse_location', type=str, help='The location (lng/lat) of the warehouse to use as the starting point for the delivery routes.')
 
     def handle(self, *args, **options):
         warehouse_location = (54.092225118165224, 12.140670094635173) # Rostock Office
         time_window = (0, 60*60*48) # 48 hours (in seconds)
         
-        vehicle_count = options['vehicle_count']
-        if vehicle_count is None or isinstance(vehicle_count, int) is False:
-            vehicle_count = Vehicle.objects.count()
+        max_vehicle_count = options['max_vehicle_count']
+        vehicle_count = min(max_vehicle_count, Vehicle.objects.filter(status='free').count())
             
         if options['warehouse_location']:
             warehouse_location = tuple(map(float, options['warehouse_location'].split(',')))
